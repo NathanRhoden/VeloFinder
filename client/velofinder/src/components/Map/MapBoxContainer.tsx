547113c9dcdf32c01b21e06c67f868de347fe6ae
Nathan <Nathan.rhoden@gmail.com>
2023-03-07 20:47:21 +0000

    Added ts version of client

---
+++ client/velofinder/src/components/Map/MapBoxContainer.tsx
@@ -0,0 +1,57 @@
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import mapboxgl from "mapbox-gl";
+import "./MapBoxContainer.css";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useRef, useEffect, useState, ReactPropTypes } from "react";
+import MAPBOXTOKEN from "../../keys/Token";
+
+mapboxgl.accessToken = MAPBOXTOKEN;
+
+const MapBoxContainer = (props : any) => {
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
+  const [zoom, setZoom] = useState(6);
+
+  const [initalised, setInitalised] = useState(false);
+
+  const getUserLocation = () => {
+    async function success(pos : any) {
+      const crd = await pos.coords;
+
+      setLat(crd.latitude);
+      setLng(crd.longitude);
+    }
+    //TODO: This must be able to catch errors // user did not  give permission to access this location
+    navigator.geolocation.getCurrentPosition(success);
+  };
+
+  const mapContainer = useRef<any>(null);
+  const map  = useRef <any> (null);
+
+  useEffect(() => {
+    getUserLocation();
+
+    if (map.current) return;
+
+    if (lat !== 0 && lng !== 0) {
+      setInitalised(true);
+    }
+
+    if (initalised) {
+      map.current = new mapboxgl.Map({
+        container: mapContainer.current,
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: [lng, lat],
+        zoom: zoom,
+      });
+    }
+  }, [lat, lng, initalised, zoom]);
+
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container"></div>
+    </div>
+  );
+};
+
+export default MapBoxContainer;

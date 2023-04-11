b864d182e8eb5ae50688515c89b7dda6f8d32df1
Nathan <Nathan.rhoden@gmail.com>
2023-04-11 06:38:02 +0100

    Added login form

---
+++ client/velofinder/src/routes/Login.tsx
@@ -0,0 +1,9 @@
+import LoginFormComponent from "../components/LoginFormComponent"
+
+export default function LoginForm() {
+    return (
+        <div>
+            <LoginFormComponent />
+        </div>
+    )
+}

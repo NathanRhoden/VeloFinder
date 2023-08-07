dfdbbc9fa0237e65d748e73139bf05f4d5d7adf2
Nathan Rhoden <61164217+NathanRhoden@users.noreply.github.com>
2023-08-07 13:55:18 +0100

    Delete velofinder directory

--- velofinder/src/main/java/com/nathanrhoden/velofinder/repository/RouteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nathanrhoden.velofinder.repository;
-
-import com.nathanrhoden.velofinder.services.entities.rider.routedata.RouteData;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RouteRepository extends JpaRepository<RouteData, Long> {
-
-}
